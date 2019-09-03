f0285929d1c84d211b10d5998535347d5c4e5417
ShafanaSafwan <kreepyprincess07@outlook.com>
2019-09-04 00:54:14 +0530

    Add shared library files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# aws-cicd-shared-lib
-This repository contains set of common functions that are used to drive the AWS Pipeline
+# Shared resources in AWS CICD Pipeline
+
+This repository contains the library resources used in AWS Pipeline. [Jenkins shared library](https://github.com/wso2/aws-cicd-shared-lib.git) contains set of functions used in [Jenkins pipeline artifacts](https://github.com/wso2/aws-cicd-pipeline.git).
+
+
+#### Prerequisites  
+This library depends on following Jenkins plugins:
+
+* [AnsiColor plugin](https://wiki.jenkins.io/display/JENKINS/AnsiColor+Plugin)
+* [Pipeline AWS Plugin](https://wiki.jenkins.io/display/JENKINS/Pipeline+AWS+Plugin)
+* [Pipeline Utility Steps Plugin](https://wiki.jenkins.io/display/JENKINS/Pipeline+Utility+Steps+Plugin)
+
+
+#### Quick Start Guide
+
+ **Step 1**: Configure this repository as a shared library under the name "wso2-jenkins-shared-lib"
+ 
+ **Step 2**: Use the functions under vars folder. Refer the text files for each function for samples.
+ 
+     
+```
+Example: Log information
+@Library('wso2-jenkins-shared-lib')
+
+node("master") {
+    stage("HelloworldExample") {
+        log.info 'helloworld'
+        log.err 'helloworld'
+    }
+}
+```
---
+++ src/vars/applyConfig.groovy
@@ -0,0 +1,62 @@
+#!/usr/bin/env groovy
+
+/*
+*  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+import org.wso2.util.Constants
+
+def call(Map config) {
+    withEnv(["ARTIFACT_LOC=${config.artifactsLoc}",
+             "ZIP_OUTPUT_LOC=${config.zipLoc}",
+             "WUM_HOME=${config.wumHome}",
+             "PUPPET_CONF_LOC=${config.puppetConfLoc}",
+             "DEPLOYMENT_PATTERN=${config.deploymentPattern}",
+             "PRODUCT=${config.product}",
+             "VERSION=${config.version}"]) {
+
+        withCredentials([usernamePassword(credentialsId: "${config.wum_creds}",
+                passwordVariable: 'WUM_PASSWORD',
+                usernameVariable: 'WUM_USERNAME')]) {
+            int status = sh(
+                    script: "${config.puppetManifest}",
+                    returnStatus: true
+            )
+            if (status == Constants.ControlConstants.STATUS_FAILED_WUM_UPDATE) {
+                throw new Exception("Failed to apply updates")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_WUM_INIT) {
+                throw new Exception("Failed to init WUM. Check your WUM credentials")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_WUM_ADD) {
+                throw new Exception("Wum add process failed")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_INPLACE_UPDATES) {
+                throw new Exception("Inplace update process failed")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_PUPPET_APPLY) {
+                throw new Exception("Puppet apply process failed")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_TO_MOVE_UPDATED_PRODUCT) {
+                throw new Exception("Failed to move updated pack")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_UNZIP) {
+                throw new Exception("Failed to unzip")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_RM_UNZIP) {
+                throw new Exception("Failed to remove zip")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_ARTIFACT_APPLY) {
+                throw new Exception("Failed to apply artifatcs")
+            } else if (status == Constants.ControlConstants.STATUS_FAILED_DOWNLOAD_PACK) {
+                throw new Exception("Failed to download product pack from S3 bucket")
+            }
+        }
+    }
+}
