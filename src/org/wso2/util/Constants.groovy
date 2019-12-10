b2326a09824bcf8d1fc6a4ae520811735fd29bfd
ShafanaSafwan <kreepyprincess07@outlook.com>
2019-12-10 14:43:57 +0530

    Add new constant

--- src/org/wso2/util/Constants.groovy
+++ src/org/wso2/util/Constants.groovy
@@ -49,7 +49,7 @@ class Constants {
         static final String DEPLOY_NETWORK = "Deploy Network Resources"
         static final String CLONE_ARTIFACT_SOURCE = "Clone artifact source repository"
         static final String MOUNT_EFS = "Mount EFS"
-        static final String ARTIFACTS = "Copy artifacts to EFS"
+        static final String COPY_ARTIFACTS = "Copy artifacts to EFS"
         static final String UNMOUNT_EFS= "Unmount EFS"
 
     }
